7807058d3ab030f892036404c4d3bb4c65c5a0df
plvhx <gandung@slackerc0de.us>
2020-12-04 16:48:58 +0700

    Refactoring main.c

--- main.c
+++ main.c
@@ -16,6 +16,14 @@ static char addition_with_first_16bit_operand[] = {
 	0x00
 };
 
+static char addition_with_second_16bit_operand[] = {
+	0x01, 0x20, /* STORE 32 */
+	0x02, 0x05, 0x78, /* STORE16 1400 */
+	0x10, /* ADD */
+	0x20, /* DISPLAY */
+	0x00
+};
+
 static void test_addition(void)
 {
 	vm_t *vm = vm_init(addition);
@@ -30,9 +38,17 @@ static void test_addition_with_first_16bit_operand(void)
 	vm_destroy(vm);
 }
 
+static void test_addition_with_second_16bit_operand(void)
+{
+	vm_t *vm = vm_init(addition_with_second_16bit_operand);
+	vm_run(vm);
+	vm_destroy(vm);
+}
+
 int main(void)
 {
 	test_addition();
 	test_addition_with_first_16bit_operand();
+	test_addition_with_second_16bit_operand();
 	return 0;
 }
