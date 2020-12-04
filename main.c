c3cd9cd5d02bfc45c27b7a9aeb1a807237f1e25a
Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
2020-12-04 13:19:09 +0000

    Refactoring main.c

--- main.c
+++ main.c
@@ -64,6 +64,30 @@ static char subtraction_with_16bit_operand_both[] = {
 	0x00
 };
 
+static char multiplication[] = {
+	0x01, 0x0a, /* STORE 10 */
+	0x01, 0x14, /* STORE 20 */
+	0x12, /* MUL */
+	0x20, /* DISPLAY */
+	0x00
+};
+
+static char multiplication_with_first_16bit_operand[] = {
+	0x02, 0x05, 0x39, /* STORE16 1337 */
+	0x01, 0x02, /* STORE 2 */
+	0x12, /* MUL */
+	0x20, /* DISPLAY */
+	0x00
+};
+
+static char multiplication_with_second_16bit_operand[] = {
+	0x01, 0x02, /* STORE 2 */
+	0x02, 0x05, 0x39, /* STORE16 1337 */
+	0x12, /* MUL */
+	0x20, /* DISPLAY */
+	0x00
+};
+
 static void test_addition(void)
 {
 	vm_t *vm = vm_init(addition);
@@ -120,6 +144,27 @@ static void test_subtraction_with_16bit_operand_both(void)
 	vm_destroy(vm);
 }
 
+static void test_multiplication(void)
+{
+	vm_t *vm = vm_init(multiplication);
+	vm_run(vm);
+	vm_destroy(vm);
+}
+
+static void test_multiplication_with_first_16bit_operand(void)
+{
+	vm_t *vm = vm_init(multiplication_with_first_16bit_operand);
+	vm_run(vm);
+	vm_destroy(vm);
+}
+
+static void test_multiplication_with_second_16bit_operand(void)
+{
+	vm_t *vm = vm_init(multiplication_with_second_16bit_operand);
+	vm_run(vm);
+	vm_destroy(vm);
+}
+
 int main(void)
 {
 	test_addition();
@@ -131,6 +176,10 @@ int main(void)
 	test_subtraction_with_first_16bit_operand();
 	test_subtraction_with_second_16bit_operand();
 	test_subtraction_with_16bit_operand_both();
+
+	test_multiplication();
+	test_multiplication_with_first_16bit_operand();
+	test_multiplication_with_second_16bit_operand();
 
 	return 0;
 }
